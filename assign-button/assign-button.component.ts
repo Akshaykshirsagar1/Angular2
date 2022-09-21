87043b9493d9b3b59c6f55727b485903307efacb
akshay <108942935+Akshaykshirsagar1@users.noreply.github.com>
2022-09-21 19:18:18 +0530

    assignment done
    assignment done

--- assign-button/assign-button.component.ts
+++ assign-button/assign-button.component.ts
@@ -1,3 +1,4 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignButtonComponent implements OnInit {
   user:string=" enter your name";
- 
+
   list=[];
-  fun1(arr){
-    this.list.push(arr.value);
-    arr.value=" "
-  }
-  fun2(ak ){
-    this.list.splice(ak,1)
-  }
   
+  
+  fun1(arr){
+    this.list.push(arr.value);//value = to get the array element
+    arr.value=" ";// to clear the input bar for next entry
+  }
+  //this. point the given array [list]
+  fun2(i ){
+    this.list.splice(i,1)// this. used for to point given array list[]... splice methode used for to delete perticular element
+  }
 
   
   constructor() { }
 
   ngOnInit() {
-    
   }
-
 }
